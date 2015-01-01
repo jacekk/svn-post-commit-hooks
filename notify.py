9b1c9371eea76a5a1c17234541230b47cfc0e77c
JacekK <info@jacek-k.net>
2015-01-02 00:28:01 +0100

    tiny refactor;

--- notify.py
+++ notify.py
@@ -3,7 +3,6 @@
 import sys, os, datetime
 import smtplib
 import commands
-
 from email.mime.text import MIMEText
 
 from config import config
@@ -29,12 +28,14 @@ def main(argv):
 			fire_notify = True
 			break
 
-	msg = "File '%s' was updated. Update your working copy." % search_path
 	if fire_notify == False:
 		sys.exit()
 
 	email_from = config["sender"]
 	email_to = argv[5]
+
+	msg = "File '%s' was updated. Update your working copy." % search_path
+	current_time = str(datetime.datetime.now())
 
 	mail = MIMEText(msg)
 	mail["Subject"] = "Repo update"
@@ -46,13 +47,14 @@ def main(argv):
 		s.login(config["smtp_user"], config["smtp_pass"])
 		s.sendmail(email_from, email_to, mail.as_string())
 		s.quit()
-		lgr.write("SENT to %s -- %s -- %s \n" % (email_to, str(datetime.datetime.now()), msg))
+		lgr.write("SENT to %s -- %s -- %s \n" % (email_to, current_time, msg))
 	except:
 		error = sys.exc_info()[1]
-		lgr.write("smtplib ERROR -- %s -- %s \n" % (str(datetime.datetime.now()), error))
+		lgr.write("smtplib ERROR -- %s -- %s \n" % (current_time, error))
 		pass
 
 	lgr.close()
+
 
 if __name__ == "__main__":
 	main(sys.argv)
